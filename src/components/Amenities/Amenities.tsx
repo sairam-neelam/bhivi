6710e159da470527778a72c3e1b9d93b3d6553c6
sairam-neelam <nvvdsairam@gmail.com>
2024-05-30 12:57:55 +0530

    amenities section

--- src/components/Amenities/Amenities.tsx
+++ src/components/Amenities/Amenities.tsx
@@ -1,5 +1,6 @@
 import { Arrow } from "../../assets";
 import "./Amenities.css";
+import { AMENITIES } from "./constants";
 
 function Amenities() {
   return (
@@ -7,6 +8,14 @@ function Amenities() {
       <div className="amenities-header">
         <h1>Why Choose us?</h1>
         <img src={Arrow} alt="arrow" />
+      </div>
+      <div className="amenities-list">
+        {AMENITIES.map((el) => (
+          <div className="amenity-card" key={el.name}>
+            <img className="amenity-logo" src={el.logo} alt={el.name} />
+            <p className="amenity-name">{el.name}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
